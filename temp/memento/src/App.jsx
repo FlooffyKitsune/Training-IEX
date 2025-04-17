9ea9b7cb69696ab0e339ebccb1deba5b6fb57bc2
Jarrett Dominic <wafflekat1123@gmail.com>
2025-04-17 18:05:18 -0400

    Started adding PWA

--- temp/memento/src/App.jsx
+++ temp/memento/src/App.jsx
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import shuffle from './utilities/shuffle';
+import { useState, useEffect } from 'react';
 import Card from './components/Card';
 import Header from './components/Header';
+import useAppBadge from './hooks/useAppBadge';
+import shuffle from './utilities/shuffle';
 
 function App() {
-  const [cards, setCards] = useState(shuffle);
-  const [pickOne, setPickOne] = useState(null);
-  const [pickTwo, setPickTwo] = useState(null);
-  const [disabled, setDisabled] = useState(false);
-  const [wins, setWins] = useState(0);
+  const [wins, setWins] = useState(0); // Win streak
+  const [cards, setCards] = useState(shuffle); // Cards array from assets
+  const [pickOne, setPickOne] = useState(null); // First selection
+  const [pickTwo, setPickTwo] = useState(null); // Second selection
+  const [disabled, setDisabled] = useState(false); // Delay handler
+  const [setBadge, clearBadge] = useAppBadge(); // Handles app badge
 
+  // Handle card selection
   const handleClick = (card) => {
     if (!disabled) {
       pickOne ? setPickTwo(card) : setPickOne(card);
@@ -20,31 +23,40 @@ function App() {
     setPickOne(null);
     setPickTwo(null);
     setDisabled(false);
-  }
+  };
 
+  // Start over
   const handleNewGame = () => {
     setWins(0);
+    clearBadge();
     handleTurn();
     setCards(shuffle);
-  }
+  };
 
+  // Used for selection and match handling
   useEffect(() => {
     let pickTimer;
 
+    // Two cards have been clicked
     if (pickOne && pickTwo) {
+      // Check if the cards the same
       if (pickOne.image === pickTwo.image) {
         setCards((prevCards) => {
           return prevCards.map((card) => {
             if (card.image === pickOne.image) {
+              // Update card property to reflect match
               return { ...card, matched: true };
             } else {
+              // No match
               return card;
             }
           });
         });
         handleTurn();
       } else {
+        // Prevent new selections until after delay
         setDisabled(true);
+        // Add delay between selections
         pickTimer = setTimeout(() => {
           handleTurn();
         }, 1000);
@@ -54,34 +66,39 @@ function App() {
     return () => {
       clearTimeout(pickTimer);
     };
-  }, [cards, pickOne, pickTwo]);
+  }, [cards, pickOne, pickTwo, setBadge, wins]);
 
+
+  // If player has found all matches, handle accordingly
   useEffect(() => {
+    // Check for any remaining card matches
     const checkWin = cards.filter((card) => !card.matched);
 
+    // All matches made, handle win/badge counters
     if (cards.length && checkWin.length < 1) {
       console.log('You win!');
       setWins(wins + 1);
+      setBadge();
       handleTurn();
       setCards(shuffle);
     }
-  }, [cards, wins]);
+  }, [cards, setBadge, wins]);
 
   return (
     <>
-
       <Header handleNewGame={handleNewGame} wins={wins} />
-
       <div className="grid">
         {cards.map((card) => {
-          const { image, id, matched } = card;
+          // Destructured card properties
+          const { image, matched } = card;
 
           return (
             <Card
-              key={id}
+              key={image.id}
+              card={card}
               image={image}
-              selected={card === pickOne || card === pickTwo || matched}
               onClick={() => handleClick(card)}
+              selected={card === pickOne || card === pickTwo || matched}
             />
           );
         })}
